2e432ef113cb7181d299032ebc9023132ece3949
Palmen01 <144669500+Palmen01@users.noreply.github.com>
2025-06-30 22:18:01 +0200

    Add-item modal

---
+++ Client/src/app/add-item-modal/add-item-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-modal',
+  imports: [],
+  templateUrl: './add-item-modal.component.html',
+  styleUrl: './add-item-modal.component.css'
+})
+export class AddItemModalComponent {
+
+
+  closeAddItemModal() {}
+
+  AddItem() {}
+}
