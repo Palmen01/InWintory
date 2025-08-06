da00fe8e2d70e5503134f3cf2e4c81ec89af4b66
Palmen01 <144669500+Palmen01@users.noreply.github.com>
2025-08-06 22:08:18 +0200

    setup item-threshold

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76411a430e271dc7b362b52f12a00b817296c0b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f282289dc60215348dec29b4d448d94c82ccb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
